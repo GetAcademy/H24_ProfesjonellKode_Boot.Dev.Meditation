cb88670357b37d837273844a10afc49c521ac1cc
dagbru <dag.sorensen@gmail.com>
2024-12-13 13:53:24 +0100

    .

--- Meditation.PrettyGooder/Program.cs
+++ Meditation.PrettyGooder/Program.cs
@@ -1,3 +1,6 @@
 ﻿using Meditation.PrettyGooder;
 
-MeditationFunctions.Meditate(10, 12, 0, 2);
+var state = Mediation.Meditate(10, 12, 0, 2);
+Console.WriteLine($"After meditation: Mana={state.CurrentMana}, " +
+                  $"Energy={state.CurrentEnergy}, " +
+                  $"Potions={state.AvailablePotions}");
---
+++ Meditation.PrettyGooder/State.cs
@@ -0,0 +1,20 @@
+﻿namespace Meditation.PrettyGooder;
+
+public class State
+{
+    public int CurrentMana { get; set; }
+    public int MaxMana { get; set; }
+    public int CurrentEnergy { get; set; }
+    public int AvailablePotions { get; set; }
+
+    public bool CanMeditate => CurrentMana < MaxMana;
+
+    public State(int currentMana, int maxMana, 
+        int currentEnergy, int availablePotions)
+    {
+        CurrentMana = currentMana;
+        MaxMana = maxMana;
+        CurrentEnergy = currentEnergy;
+        AvailablePotions = availablePotions;
+    }
+}
